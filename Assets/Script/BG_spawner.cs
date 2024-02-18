f442fe7d003d787cb2928a1440bce9275542a589
SugaRush2077 <ussr0408@gmail.com>
2024-02-18 15:03:46 -0500

    add background object

---
+++ Assets/Script/BG_spawner.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG_spawner : MonoBehaviour
+{
+    [System.Serializable]
+    public struct SpawnableObject
+    {
+        public GameObject prefab;
+        [Range(0f, 1f)]
+        public float spawnChance;
+    }
+
+    public SpawnableObject[] objects;
+
+    // Spawn time in second
+    // Depend on distance & speed & time
+    public float minSpawnRate = 0.5f;
+    public float maxSpawnRate = 1f;
+    //private float spawnPeriod = 1f;
+    
+      
+    private void OnEnable()
+    {
+        //Invoke(nameof(Spawn), Random.Range(minSpawnRate, maxSpawnRate));
+    }
+
+
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+    private void Spawn()
+    {
+        float spawnProbability = Random.value;
+
+        foreach (var obj in objects)
+        {
+            if (spawnProbability < obj.spawnChance)
+            {
+                GameObject background = Instantiate(obj.prefab);
+                background.transform.position += transform.position;
+                break;
+            }
+
+            spawnProbability -= obj.spawnChance;
+        }
+        Invoke(nameof(Spawn), Random.Range(minSpawnRate, maxSpawnRate));
+        
+    }
+}
---
+++ Assets/Script/Background.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    private float leftEdge;
+
+    private void Start()
+    {
+        leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 4f;
+    }
+    private void Update()
+    {
+        transform.position += Vector3.left * GameManager.Instance.gameSpeed * Time.deltaTime;
+
+        if (transform.position.x < leftEdge)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
